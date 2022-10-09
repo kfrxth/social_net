bbf00c7c46d3b73d970b0acf4752e398cbff0e59
Luck4eR <kfrxth@gmail.com>
2022-10-09 18:59:29 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://lh3.googleusercontent.com/a/ALm5wu06UWAKeeXkUCkBNaz45GGE3ywF-1P3sgTZGV4q=s576-p-rw-no"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/luck4er-profile_banner-e0a6139f2bbbac3a-480.jpeg"></img>
+      </div>
+      <div>ава + описание</div>
+      <div>
+        мои посты
+        <div>Новый пост</div>
+      </div>
+      <div>
+        <div>Пост 1</div>
+        <div>Пост 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
