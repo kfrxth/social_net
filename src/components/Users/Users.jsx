dda7f3f667bdebf3c798dfd28a2269f124d98fff
Luck4eR <kfrxth@gmail.com>
2022-12-13 16:47:56 +0300

    pagination + dialogs

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,75 +1,69 @@
-import axios from "axios";
 import React from "react";
 import styles from "./users.module.css";
 
-const userPhoto =
-  "https://png.clipart.me/istock/previews/1929/19298171-funny-cartoon-office-worker.jpg";
-const apiAddress = "https://social-network.samuraijs.com/api/1.0/users?term=g";
+const Users = (props) => {
+  const userPhoto =
+    "https://png.clipart.me/istock/previews/1929/19298171-funny-cartoon-office-worker.jpg";
+  const pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.users.length === 0) {
-      axios.get(apiAddress).then((response) => {
-        this.props.setUsers(response.data.items);
-      });
-    }
+  let pages = [];
+
+  for (let i = 1; i <= pageCount; i++) {
+    pages.push(i);
   }
 
-  render() {
-    const pageCount = this.props.totalUsersCount / this.props.pageSize;
-
-    let pages = [];
-
-    for (let i = 1; i <= pageCount; i++) {
-      pages.push(i);
-    }
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          {pages.map((i) => {
-            return <span className={this.props.currentPage === i ? styles.selectedPage : styles.span}>{i}</span>;
-          })}
-        </div>
-        {this.props.users.map((user) => (
-          <div key={user.id}>
-            <span>
-              <div>
-                <img
-                  src={
-                    user.photos.small !== null ? user.photos.small : userPhoto
-                  }
-                  alt="img"
-                  className={styles.userAvatar}
-                ></img>
-              </div>
-              <div>
-                {user.followed ? (
-                  <button onClick={() => this.props.unfollow(user.id)}>
-                    Отписаться
-                  </button>
-                ) : (
-                  <button onClick={() => this.props.follow(user.id)}>
-                    Подписаться
-                  </button>
-                )}
-              </div>
+        {pages.map((i) => {
+          return (
+            <span
+              className={
+                props.currentPage === i ? styles.selectedPage : styles.span
+              }
+              onClick={() => props.onPageChanged(i)}
+            >
+              {i}
             </span>
-            <span>
-              <span>
-                <div>{user.name}</div>
-                <div>user.job.department</div>
-              </span>
-              <span>
-                <div>user.job.post</div>
-                <div>user.status</div>
-              </span>
-            </span>
-          </div>
-        ))}
+          );
+        })}
       </div>
-    );
-  }
-}
+      {props.users.map((user) => (
+        <div key={user.id}>
+          <span>
+            <div>
+              <img
+                src={user.photos.small !== null ? user.photos.small : userPhoto}
+                alt="img"
+                className={styles.userAvatar}
+              ></img>
+            </div>
+            <div>
+              {user.followed ? (
+                <button onClick={() => props.unfollow(user.id)}>
+                  Отписаться
+                </button>
+              ) : (
+                <button onClick={() => props.follow(user.id)}>
+                  Подписаться
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{user.name}</div>
+              <div>user.job.department</div>
+            </span>
+            <span>
+              <div>user.job.post</div>
+              <div>user.status</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Users;
