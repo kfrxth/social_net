ea23f41a0111c5ee8fc3a2c25199a0a7c381ea81
Luck4eR <kfrxth@gmail.com>
2023-02-11 20:22:02 +0300

    redux+react extens

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose, combineReducers, legacy_createStore as createStore } from "redux";
 import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
@@ -18,6 +18,7 @@ let reducers = combineReducers({
 	app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
