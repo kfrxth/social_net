f0941c9923245d62f1ccf826f98fda03d2b6a6bf
Luck4eR <kfrxth@gmail.com>
2023-03-12 19:41:58 +0300

    Updates

---
+++ precache-manifest.1d52ca0c264f8bfad89706432325bcc6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "48d563a3e3b02fa71322",
+    "url": "/social_net/static/js/0.48d563a3.chunk.js"
+  },
+  {
+    "revision": "a6f63b390e8e21f5086f",
+    "url": "/social_net/static/css/main.19dddeb3.chunk.css"
+  },
+  {
+    "revision": "a6f63b390e8e21f5086f",
+    "url": "/social_net/static/js/main.a6f63b39.chunk.js"
+  },
+  {
+    "revision": "ac4195ad5f916bdfcbe9",
+    "url": "/social_net/static/js/2.ac4195ad.chunk.js"
+  },
+  {
+    "revision": "bfb0f1fc7c1195229509",
+    "url": "/social_net/static/css/3.cbe6221e.chunk.css"
+  },
+  {
+    "revision": "bfb0f1fc7c1195229509",
+    "url": "/social_net/static/js/3.bfb0f1fc.chunk.js"
+  },
+  {
+    "revision": "2f23c29c8a8692b9a3df",
+    "url": "/social_net/static/css/4.45f39c8f.chunk.css"
+  },
+  {
+    "revision": "2f23c29c8a8692b9a3df",
+    "url": "/social_net/static/js/4.2f23c29c.chunk.js"
+  },
+  {
+    "revision": "7f2648944b6ada6c3fc6",
+    "url": "/social_net/static/css/5.b8e68d58.chunk.css"
+  },
+  {
+    "revision": "7f2648944b6ada6c3fc6",
+    "url": "/social_net/static/js/5.7f264894.chunk.js"
+  },
+  {
+    "revision": "632f57a5192a0a326c1d",
+    "url": "/social_net/static/css/6.d81e3e44.chunk.css"
+  },
+  {
+    "revision": "632f57a5192a0a326c1d",
+    "url": "/social_net/static/js/6.632f57a5.chunk.js"
+  },
+  {
+    "revision": "ce830c0db0a05a6fc16c",
+    "url": "/social_net/static/js/7.ce830c0d.chunk.js"
+  },
+  {
+    "revision": "30e62ebf8ca39a9a1fd7",
+    "url": "/social_net/static/js/runtime~main.30e62ebf.js"
+  },
+  {
+    "revision": "5794fa3f13beb4d8660c3b29543d955b",
+    "url": "/social_net/static/media/loader.5794fa3f.gif"
+  },
+  {
+    "revision": "0bca66a258818138fd00cb9e45dbba37",
+    "url": "/social_net/index.html"
+  }
+];
